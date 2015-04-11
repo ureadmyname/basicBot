07047ba46291b50975f50f1064cfa662fa10ab42
ureadmyname <ureadmyemailname@gmail.com>
2015-04-11 11:01:55 +0930

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -54,7 +54,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/ureadmyname/basicBot/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
