497adde8b5cad59aee21c1571d5d90f777a87187
Jason <ureadmyemailname@gmail.com>
2015-05-08 13:05:18 +0930

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1808,6 +1808,47 @@
                 }
             },
 
+shotsCommand: {
+                command: 'shots',
+                rank: 'user',
+                type: 'startsWith',
+                shots: ['Whiskey',
+                    'Smirnoff',
+                    'Jim Beam',
+                    'Jack Daniels',
+                    'Johnny Walker'
+                ],
+                getShots: function () {
+                    var sho = Math.floor(Math.random() * this.shots.length);
+                    return this.shots[sho];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatshot);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousershots, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfshots, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.shots, {nameto: user.username, namefrom: chat.un, shots: this.getShots()}));
+                            }
+                        }
+                    }
+                }
+            },
+
             cookieCommand: {
                 command: 'cookie',
                 rank: 'user',
