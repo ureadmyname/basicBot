8f290bc22aae58f8ab25855627e8949c0f984c26
Jason <ureadmyemailname@gmail.com>
2015-05-31 19:19:38 +0930

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -783,14 +783,22 @@
                     }, 1 * 1000, user);
             }
         },
-        eventUserleave: function (user) {
-            for (var i = 0; i < basicBot.room.users.length; i++) {
-                if (basicBot.room.users[i].id === user.id) {
-                    basicBot.userUtilities.updateDC(basicBot.room.users[i]);
-                    basicBot.room.users[i].inRoom = false;
-                }
-            }
-        },
+         eventUserleave: function (user) {
+            var previousDJ = API.getHistory()[1].user.id;
+            var lastDJ = API.getHistory()[0].user.id;
+             for (var i = 0; i < basicBot.room.users.length; i++) {
+                 if (basicBot.room.users[i].id === user.id) {
+                     basicBot.userUtilities.updateDC(basicBot.room.users[i]);
+                     basicBot.room.users[i].inRoom = false;
+                    if (lastDJ == user.id || previousDJ == user.id){
+                        var user = basicBot.userUtilities.lookupUser(basicBot.room.users[i].id);
+                        basicBot.userUtilities.updatePosition(user, 0);
+                        user.lastDC.time = null;
+                        user.lastDC.position = user.lastKnownPosition;
+                    }
+                 }
+             }
+         },
         eventVoteupdate: function (obj) {
             for (var i = 0; i < basicBot.room.users.length; i++) {
                 if (basicBot.room.users[i].id === obj.user.id) {
